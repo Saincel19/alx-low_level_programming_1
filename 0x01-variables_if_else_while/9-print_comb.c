44baf9592a29b5691f4c41cb47ac805493ac2269
Ezemonye Michael <ezemonyemichael@gmail.com>
2023-02-20 11:28:13 -0800

    second commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * Description: 'print comb of numbers'
+ * Return: Always 0
  */
 
 int main(void)
 {
 	int num;
-	char la;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-	for (la = 'a'; la <= 'f'; la++)
-		putchar(la);
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
-
 	return (0);
 }
