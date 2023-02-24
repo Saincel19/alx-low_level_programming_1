7eebe906e75b289b9ac2f6375a1a0092775ee628
Ezemonye Michael <ezemonyemichael@gmail.com>
2023-02-24 05:48:27 -0800

    second commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
- * _isupper - checks for uppercase character
- * @c: Variable text
- * Return: 0 Always (success)
- */
-
-int _isupper(int c);
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
