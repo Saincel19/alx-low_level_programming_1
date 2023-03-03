f9b05f3f46fda7c644ec472843b16ac2eb35f18c
Ezemonye Michael <ezemonyemichael@gmail.com>
2023-03-03 10:37:27 -0800

    commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,4 @@
 #include <stdio.h>
-/*
-*main - add one line of code
-* Return: 0 always (success)
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
 int main(void)
 {
 	int n;
@@ -16,6 +7,13 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+/*
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
 	*(p + 5) = 98;
 /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
